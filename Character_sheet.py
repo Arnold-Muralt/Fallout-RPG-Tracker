0498c182906569ebaf5a1127ca7755374979c803
Arnold-Muralt <42650495+Arnold-Muralt@users.noreply.github.com>
2024-08-09 23:45:15 -0400

    Create Character_sheet.py

---
+++ Character_sheet.py
@@ -0,0 +1,27 @@
+!Base special Characteristics
+
+Strength = 1
+Perception =1
+Endurance = 1
+Charisma = 1
+Intelligence =1
+Agility = 1
+Luck =1 
+total_points = 
+! Skills
+Melee = Strength * 5 + Luck
+Small_Guns = Agility  * 5 + Luck
+Big_Guns = (Endurance+Strength)/2  * 5 + Luck
+Energy_Weapons = (Intelligence+Agility)/2 * 5 + Luck
+Long_Guns = (Perception+Agility)/2 * 5 + Luck
+Explosives = (Agility+Perception)/2 * 5 + Luck
+Repair = (Intelligence+Agility)/2 * 5 + Luck
+Lockpick =(Intelligence+Agility)/2 * 5 + Luck
+Science = Intelligence * 5 + Luck
+Bartering = Charisma * 5 + Luck
+Speech = Charisma * 5 + Luck
+Medic = (Intelligence +Charisma)/2 * 5 + Luck
+Sneak = Agility * 5 + Luck
+Observe = Perception * 5 + Luck
+Survival = Endurance * 5 + Luck
+
